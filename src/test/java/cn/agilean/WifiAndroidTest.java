03d8b823928e2896cffb7cbb4a5df73638af3d9a
zxgjava <zxgjava@163.com>
2016-04-20 22:04:54 +0800

    Add wifi test

---
+++ src/test/java/cn/agilean/WifiAndroidTest.java
@@ -0,0 +1,83 @@
+package cn.agilean;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+public class WifiAndroidTest {
+
+    private AppiumDriver<WebElement> driver;
+
+    @Before
+    public void setUp() throws Exception {
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "apps");
+        File app = new File(appDir, "PINGAN_WIFI_ANDROID.apk");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName","Android Emulator");
+        capabilities.setCapability("platformVersion", "4.4");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        capabilities.setCapability("appPackage", "com.pingan.pinganwifi");
+        capabilities.setCapability("appActivity", ".loading.LoadingActivity");
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void wifiDemo(){
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        WebElement el = driver.findElement(By.name("立即体验"));
+        assertEquals("立即体验", el.getText());
+        el.click();
+
+        el = driver.findElement(By.name("我"));
+        assertEquals("我", el.getText());
+        el.click();
+
+        el = driver.findElement(By.name("注册"));
+        assertEquals("注册", el.getText());
+        el.click();
+
+        el = driver.findElement(By.name("登录"));
+        assertEquals("登录", el.getText());
+        el.click();
+
+        // find user-name input field
+        WebElement userNameInput = driver.findElement(By.id("login_phone_edit"));
+        wait.until(ExpectedConditions.visibilityOf(userNameInput));
+
+        // type user-name in input field
+        userNameInput.clear();
+        userNameInput.sendKeys("15834064949");
+        driver.findElement(By.id("login_password_edit")).sendKeys("15834064949");
+
+        // submit login form
+        driver.findElement(By.name("立即登录")).click();
+
+        List<WebElement> els = driver.findElementsByClassName("android.widget.TextView");
+        System.out.println("els:"+els.get(0).getText());
+        wait.until(ExpectedConditions.visibilityOf(els.get(0)));
+        //assertEquals("Activity", els.get(2).getText());
+    }
+
+}
